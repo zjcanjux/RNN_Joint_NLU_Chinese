3d0d438bf65f090265b3f8e39a209ff9eda3ebd5
zjcanjux <37158421+zjcanjux@users.noreply.github.com>
2019-03-02 10:04:04 +0800

    Update B_I_slot_label.py

--- B_I_slot_label.py
+++ B_I_slot_label.py
@@ -11,7 +11,7 @@ class Slot_label(object):
     def __init__(self, input_file_path, output_file_path):
         self.input_file_path = input_file_path
         self.output_file_path = output_file_path
-
+        self.B_I_label()
     def B_I_label(self):
         f = open(self.input_file_path, 'r').readlines()
         seq_cut_total = []
@@ -127,4 +127,3 @@ class Slot_label(object):
 
 
 aa = Slot_label(input_file_path, output_file_path)
-aa.B_I_label()
